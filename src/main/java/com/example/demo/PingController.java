cf4e6458959219f9ca78f51cecfa0bc06ec47ba4
justasn88 <justasn88@gmail.com>
2026-08-12 20:06:25 +0300

    optimize imports

--- src/main/java/com/example/demo/PingController.java
+++ src/main/java/com/example/demo/PingController.java
@@ -2,8 +2,6 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @RestController
 public class PingController {
