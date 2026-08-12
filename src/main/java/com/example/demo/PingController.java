781ade7618916b0ef63e3ce43fac10f1ed1abb51
justasn88 <justasn88@gmail.com>
2026-08-12 20:14:35 +0300

    Added comment

--- src/main/java/com/example/demo/PingController.java
+++ src/main/java/com/example/demo/PingController.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+// comment
 @RestController
 public class PingController {
 
